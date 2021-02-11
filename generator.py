a1a35f64adb0220878e9a0f88c13920f4b882c65
totu <topituulensuu@fastmail.com>
2021-02-11 17:43:31 +0200

    renamte writer to generator and add few letters

--- generator.py
+++ generator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
+"""This script generates daily schedule for commits as list of booleans"""
 
-def write_to_grid(string):
+def generate(string):
     """Returns commit schedule as list of Booleans"""
     # Letters are 4x5 (this leaves 1 space above and below)
     # TODO: Add support for more letters
@@ -9,6 +10,10 @@ def write_to_grid(string):
         "B": ["##  ", "# # ", "##  ", "# # ", "##  "],
         "C": [" ## ", "#   ", "#   ", "#   ", " ## "],
         "D": ["##  ", "# # ", "# # ", "# # ", "##  "],
+        "E": ["### ", "#   ", "### ", "#   ", "### "],
+        "H": ["# # ", "# # ", "### ", "# # ", "# # "],
+        "L": ["#   ", "#   ", "#   ", "#   ", "### "],
+        "O": ["### ", "# # ", "# # ", "# # ", "### "],
     }
 
     # Add padding for the extra lines above and below
@@ -21,7 +26,6 @@ def write_to_grid(string):
             commit_days += eval(",".join([x[i] for x in letter]).replace(" ", "False").replace("#", "True"))
     return commit_days
 
-
 if __name__ == "__main__":
-    days = write_to_grid("ABCD")
-    print(days)
+    instructions = generate("HELLO")
+    print(instructions)
